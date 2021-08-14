299d3ee42a68fa3d72525750ddb31cf3a8936ece
wonglok <yellowhappy831@gmail.com>
2021-08-15 05:26:20 +0800

    letsgo

---
+++ vfx/firebaseConfig.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAPuwK2yMl025KLVTnGDdN34XxHBYQHoVk',
+  authDomain: 'effect-node-by-you.firebaseapp.com',
+  databaseURL: 'https://en-you.firebaseio.com/',
+  projectId: 'effect-node-by-you',
+  storageBucket: 'effect-node-by-you.appspot.com',
+  messagingSenderId: '587774316246',
+  appId: '1:587774316246:web:2a52c46bd184fc3a1d4377',
+  measurementId: 'G-SYFGKL6VNR'
+};
