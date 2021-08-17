7e087688ad74d84ac8d6cb57b32a6bba312bbf21
wonglok <yellowhappy831@gmail.com>
2021-08-17 12:08:41 +0800

    3d world

--- vfx-metaverse/compos/StarSky.js
+++ vfx-metaverse/compos/StarSky.js
@@ -17,7 +17,7 @@ export function StarSky() {
     }
     `,
     fragmentShader: `
-      precision mediump float;
+      precision highp float;
 
       vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
       vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
